2b39d73b6887d94db75ae1515f30165e90adc6df
NLoguin <nucleologuin@nucleologuin.com.br>
2023-01-28 02:05:35 -0300

    Acerto de erro de versão do AspNetCore

---
+++ Fontes/NL.GI.ERP.Customizados/Controladores/CustomizadosExemploController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NL.GI.ERP.Extensoes.API;
+
+namespace NL.GI.ERP.Customizados.Controladores
+{
+    [Route("api/v1/customizados/[controller]")]
+    public class CustomizadosExemploController : NLGIERPAPIControllerPing_1_0
+    {
+        public CustomizadosExemploController(ILogger<CustomizadosExemploController> logger) 
+            : base(logger)
+        {
+        }
+    }
+}
