e7ec88068508cae5a57a18fbf1026dd315f3d01d
Victor Moor <victor.moor@live.com>
2023-02-01 14:59:43 -0300

    Criando DataContext

---
+++ Fontes/NL.GI.ERP.Customizados/Controladores/AFI/OlucroVendasController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NL.GI.ERP.Extensoes.API;
+
+namespace NL.GI.ERP.Customizados.Controladores
+{
+    [Route("api/v1/customizados/AFI/[controller]")]
+    public class OlucroVendasController : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var vendas = new List<OlucroVendas>
+        {
+            new OlucroVendas
+            {
+                Cod = 1 ,
+                Qtda = 100,
+                Preco = 200,
+                ICMS = 300,
+                ICMS_ST = 500,
+                IPI = 600,
+            }
+        };
+
+
+            return Ok(vendas);
+
+        }
+    }
+}
---
+++ Fontes/NL.GI.ERP.Customizados/OlucroVendas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NL.GI.ERP.Customizados
+{
+    internal class OlucroVendas
+    {
+        public int Cod { get; set; }
+        public int Qtda { get; set;}
+        public int Preco { get; set;}
+        public int ICMS { get; set;}
+        public int ICMS_ST { get; set;}
+        public int IPI { get; set;}
+    }
+    
+}
